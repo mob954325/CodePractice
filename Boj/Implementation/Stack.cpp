91b9595574dfeaf616da2ba9837541dbd99ebbe1
mob954325 <lsh954325@gmail.com>
2024-04-16 09:42:11 +0900

    Update Stack.cpp

--- Boj/Implementation/Stack.cpp
+++ Boj/Implementation/Stack.cpp
@@ -3,42 +3,95 @@
 /// 
 
 #include <iostream>
+#define MAX_STACK_SIZE 10000
 
 using namespace std;
 
+int arr[MAX_STACK_SIZE];
+
 class Stack
 {
+private:
+	int pointer = 0;
 public:
-	Stack() { cout << "积己磊 积己凳\n"; };
-	~Stack() { cout << "积己磊 力芭凳"; };
-
-	int push()
+	void init()
 	{
-		return 0;
+		for (int i = 0; i < MAX_STACK_SIZE; i++)
+		{
+			arr[i] = NULL;
+		}
 	}
 
-	void pop()
+	void push(int value)
 	{
+		pointer++;
+		arr[pointer] = value;
+	}
 
+	int pop()
+	{
+		int popValue = arr[pointer];
+		arr[pointer] = NULL;
+
+		pointer > 0 ? pointer-- : pointer = 0;
+
+		return popValue != NULL ? popValue : -1;
 	}
 
 	int size()
 	{
-		return 0;
+		return pointer;
 	}
 
 	int empty()
 	{
-		return 0;
+		return pointer > 0 ? 0 : 1;
 	}
 
 	int top()
 	{
-		return 0;
+		return pointer > 0 ? arr[pointer] : -1;
 	}
 };
 
 int main()
 {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
 	Stack s;
+
+	int inputCount;
+	cin >> inputCount;
+
+	for (int i = 0; i < inputCount; i++)
+	{
+		string command;
+		int value;
+
+		cin >> command;
+
+		if (command == "push")
+		{
+			cin >> value;
+			s.push(value);
+		}
+		else if (command == "pop")
+		{
+			cout << s.pop() << "\n";
+		}
+		else if (command == "size")
+		{
+			cout << s.size() << "\n";
+		}
+		else if (command == "empty")
+		{
+			cout << s.empty() << "\n";
+		}
+		else if (command == "top")
+		{
+			cout << s.top() << "\n";
+		}
+	}
 }
