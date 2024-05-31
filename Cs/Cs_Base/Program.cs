c63a9f4b1db67e22d88264bac7c4913c341aa347
mob954325 <lsh954325@gmail.com>
2024-05-31 15:18:15 +0900

    add Recursive Backtracking.cs

--- Cs/Cs_Base/Program.cs
+++ Cs/Cs_Base/Program.cs
@@ -10,7 +10,39 @@ namespace Cs_Base
     {
         static void Main(string[] args)
         {
-            // program execution 
+            int width;
+            int height;
+
+            Cell[,] cells;
+
+            // 입력 및 초기화 
+            string str = Console.ReadLine();
+            width = Convert.ToInt32(str);
+
+            str = Console.ReadLine();
+            height = Convert.ToInt32(str);
+
+            cells = new Cell[height, width];
+            
+            // 미로 셀 생성
+            for(int y = 0; y < height; y++)
+            {
+                for(int x = 0; x < width; x++)
+                {
+                    cells[y, x] = new Cell();
+                }
+            }
+
+            // 미로 생성 시작
+            Recursive_backtracking maze = new(width, height, cells);
+
+            int startPosition = maze.GetStartPosition();
+
+            int startX = startPosition % width;
+            int startY = startPosition / height;
+
+            cells[startX, startY].visited = true;
+            maze.StartRecursive(startX, startY);
         }
     }
 }
