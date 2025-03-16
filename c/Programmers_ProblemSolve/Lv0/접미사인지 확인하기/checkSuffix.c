ea0a7fae70322d9c90ee566c7130d3de2c5ccf54
Lee SeongHo <lsh954325@gmail.com>
2025-03-16 14:10:02 +0900

    Update checkSuffix.c

--- c/Programmers_ProblemSolve/Lv0/접미사인지 확인하기/checkSuffix.c
+++ c/Programmers_ProblemSolve/Lv0/접미사인지 확인하기/checkSuffix.c
@@ -8,7 +8,6 @@
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* my_string, const char* is_suffix) {
-    // 접미사 저장
     int strLen = strlen(my_string);
     int suffixLen = strlen(is_suffix);
 
