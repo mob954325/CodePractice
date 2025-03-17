cfb9a7b809b6ffd671c35a325c8188fe0ab2f764
Lee SeongHo <lsh954325@gmail.com>
2025-03-17 22:13:37 +0900

    Create Program.cs

---
+++ cs/Basic/LocalFunction/Program.cs
@@ -0,0 +1,19 @@
+namespace LocalFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void aa()
+            {
+                void bb()
+                {
+                    void cc()
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
