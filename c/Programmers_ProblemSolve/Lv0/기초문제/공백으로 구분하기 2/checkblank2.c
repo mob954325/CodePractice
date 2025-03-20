be77467e53831a4aecc09594f1169ea86df77c53
Lee SeongHo <lsh954325@gmail.com>
2025-03-20 19:27:56 +0900

    Update checkblank2.c

--- c/Programmers_ProblemSolve/Lv0/기초문제/공백으로 구분하기 2/checkblank2.c
+++ c/Programmers_ProblemSolve/Lv0/기초문제/공백으로 구분하기 2/checkblank2.c
@@ -1,12 +1,12 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MAXSIZE 1024
+
+// 2 7 8 : aborted
+// 4 : fail
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char** solution(const char* my_string) {
@@ -15,18 +15,18 @@ char** solution(const char* my_string) {
 
     int wordCount = 0;
     char* word = strtok(my_string, " ");
-
     while (word != NULL)
     {
-        wordContainer[wordCount] = (char*)malloc(strlen(word) * sizeof(char));
-        wordContainer[wordCount++] = word;
+        wordContainer[wordCount] = (char*)malloc((strlen(word) + 1) * sizeof(char));
+        strcpy(wordContainer[wordCount], word);
+        wordCount++;
         word = strtok(NULL, " ");
     }
 
     char** answer = (char**)malloc(wordCount * sizeof(char*));
     for (int i = 0; i < wordCount; i++)
     {
-        answer[i] = (char*)malloc(strlen(wordContainer[i]) * sizeof(char));
+        answer[i] = (char*)malloc((strlen(wordContainer[i]) + 1) * sizeof(char));
         strcpy(answer[i], wordContainer[i]);
     }
 
