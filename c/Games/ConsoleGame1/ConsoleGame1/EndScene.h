431efb6b5f866edce95730c82d331300d94baa01
Lee SeongHo <lsh954325@gmail.com>
2025-03-29 16:34:44 +0900

    콘솔 게임 프로젝트 분리
    + 리포지토리 따로 만듬

--- c/Games/ConsoleGame1/ConsoleGame1/EndScene.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "EndSceneUI.h"
-
-namespace EndScene
-{
-	void Initialize();
-	void Update();
-	void Render();
-}
--- c/Games/ConsoleGame1/ConsoleGame1/MenuScene.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "MenuSceneUI.h"
-
-namespace MenuScene
-{
-	void Initialize();
-	void Update();
-	void Render();
-}
