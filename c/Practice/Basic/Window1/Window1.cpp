aad8dac81b11a9ab46a30515e076464af5276e7e
mob954325 <lsh954325@gmail.com>
2025-04-03 14:01:10 +0900

    windows api 창 만들기 업로드

---
+++ c/Practice/Basic/Window1/Window1.cpp
@@ -0,0 +1,79 @@
+﻿#ifndef UNICODE
+#define UNICODE
+#endif 
+
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    // Register the window class.
+    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+    WNDCLASS wc = { };  // RegisterClass에 등록된 창의 정보 구조체
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc); // CreateWindow나 CreateWindowEx에 등록된 창 정보 불러오기
+
+    // 창 만들기
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        L"Learn to Program Windows",    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow); 
+
+    // 메세지 루프 ( 쓰래드 메세지 큐 처리 )
+    MSG msg = { };  // 메세지 구조체
+    while (GetMessage(&msg, NULL, 0, 0) > 0) // WM_QUIT인지 확인
+    {
+        TranslateMessage(&msg); // 가상 키 베세지를 문자 메세지로 변환
+        DispatchMessage(&msg);  // 메세지 디스패치
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY: // 창 종료
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:  // 그리기
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        // All painting occurs here, between BeginPaint and EndPaint.
+
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+        EndPaint(hwnd, &ps);
+    }
+    return 0;
+
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
