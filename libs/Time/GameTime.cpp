1a0be16bb80457f26fd0fffa5f9b6d27a29bdb08
Lee SeongHo <lsh954325@gmail.com>
2025-04-06 21:48:10 +0900

    Time 라이브러리 이름 변경
    Time -> Gametime
    이유 - 기존 time.h 랑 이름이 겹치기 때문에 직관적으로 변경함

--- libs/Time/GameTime.cpp
+++ libs/Time/GameTime.cpp
@@ -1,11 +1,11 @@
 ﻿#include "framework.h"
-#include "Time.h"
+#include "GameTime.h"
 
 LARGE_INTEGER freq, curr, prev, start, end;
 float deltaTime;
 float timeElapsed;
 
-void Time::InitTime()
+void GameTime::InitTime()
 {
     QueryPerformanceFrequency(&freq); // QPC 주파수(초당 Tick 수) 가져오기
     QueryPerformanceCounter(&start);
@@ -14,19 +14,19 @@ void Time::InitTime()
     timeElapsed = 0;
 }
 
-void Time::UpdateTime()
+void GameTime::UpdateTime()
 {
     QueryPerformanceCounter(&curr);
     deltaTime = static_cast<float>(curr.QuadPart - prev.QuadPart) / freq.QuadPart;
     prev = curr;
 }
 
-float Time::GetDeltaTime()
+float GameTime::GetDeltaTime()
 {
     return deltaTime;
 }
 
-float Time::GetElapsedTime()
+float GameTime::GetElapsedTime()
 {
     QueryPerformanceCounter(&end);
     return static_cast<float>(end.QuadPart - start.QuadPart) / freq.QuadPart;
