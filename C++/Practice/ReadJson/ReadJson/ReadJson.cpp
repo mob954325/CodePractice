f19ade94f86e99fcc42a07157310a1852969a7b2
mob954325 <lsh954325@gmail.com>
2025-04-29 17:28:57 +0900

    json 파일 읽기 추가

---
+++ C++/Practice/ReadJson/ReadJson/ReadJson.cpp
@@ -0,0 +1,44 @@
+﻿// https://github.com/nlohmann/json/releases
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+#include <vector>
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+int main()
+{
+	std::ifstream file("Stage1.json", std::ios::in);
+
+	if (!file.is_open())
+	{
+		std::cerr << "Error opening Stage1.json \n";
+		return 1;
+	}
+
+	json j;
+	file >> j;
+
+	std::vector<int> stage_vector;
+
+	// layer에서 height의 요소 출력
+	for (const auto& result : j["layers"])
+	{
+		int curr = result["height"].get<int>();
+		printf("%d\n", curr);
+	}
+
+	for (const auto& layer : j["layers"])
+	{
+		const auto& data = layer["data"];
+		stage_vector.insert(stage_vector.end(), data.begin(), data.end());
+	}
+
+	for (const auto& value : stage_vector)
+	{
+		std::cout << value << " ";
+	}
+}
